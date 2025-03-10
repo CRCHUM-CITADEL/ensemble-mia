53cb9095aaf21de10043c46fe52186762d49b659
Yue Qi <yue.qi.chum@ssss.gouv.qc.ca>
2025-03-10 17:07:15 -0400

    Update function

--- src/data/process.py
+++ src/data/process.py
@@ -74,6 +74,7 @@ def generate_val_test(
     df_real_train: pd.DataFrame,
     df_real_control_val: pd.DataFrame,
     df_real_control_test: pd.DataFrame,
+    stratify: pd.Series,
     seed: int,
 ) -> Tuple[pd.DataFrame, np.ndarray, pd.DataFrame, np.ndarray]:
     """Generate the validation and test set with labels
@@ -81,40 +82,60 @@ def generate_val_test(
     :param df_real_train: the real train data
     :param df_real_control_val: the real control data for validation
     :param df_real_control_test: the real control data for final evaluation
+    :param stratify: a pandas series for stratify the real train data
     :param seed: for reproduction
 
     :return: the features and label for validation and test sets
     """
+    df_real_train["stratify"] = stratify
 
     # Construct validation set for ensemble model
-    df_real_train_sample = df_real_train.sample(
-        n=len(df_real_control_val) + len(df_real_control_test),
-        replace=False,
-        ignore_index=True,
+    df_real_train_val, df_temp = train_test_split(
+        df_real_train,
+        train_size=len(df_real_control_val),
+        stratify=df_real_train["stratify"],
         random_state=seed,
     )
 
-    df_real_train_val = df_real_train_sample.iloc[: len(df_real_control_val), :]
+    #  Label 1 for real records used to generate 1st generation synthetic data and 0 for control
+    df_real_train_val["is_train"] = 1
+    df_real_control_val["is_train"] = 0
 
     df_val = pd.concat(
-        [df_real_train_val, df_real_control_val],
+        [df_real_train_val.drop(columns=["stratify"]), df_real_control_val],
         axis=0,
         ignore_index=True,
     )
 
-    #  Label 1 for real records used to generate 1st generation synthetic data and 0 for control
-    y_val = np.array([1] * len(df_real_train_val) + [0] * len(df_real_control_val))
+    # Shuffle data
+    df_val = df_val.sample(frac=1, random_state=seed).reset_index(drop=True)
+
+    y_val = df_val["is_train"].values
+    df_val = df_val.drop(columns=["is_train"])
 
     # Test set: can be used to evaluate all the models
-    df_real_train_test = df_real_train_sample.iloc[len(df_real_control_val) :, :]
+    if len(df_temp) == len(df_real_control_test):
+        df_real_train_test = df_temp
+    else:
+        df_real_train_test, _ = train_test_split(
+            df_temp,
+            train_size=len(df_real_control_test),
+            stratify=df_temp["stratify"],
+            random_state=seed,
+        )
+
+    df_real_train_test["is_train"] = 1
+    df_real_control_test["is_train"] = 0
 
     df_test = pd.concat(
-        [df_real_train_test, df_real_control_test],
+        [df_real_train_test.drop(columns=["stratify"]), df_real_control_test],
         axis=0,
         ignore_index=True,
     )
 
-    #  Label 1 for real records used to generate 1st generation synthetic data and 0 for control.
-    y_test = np.array([1] * len(df_real_train_test) + [0] * len(df_real_control_test))
+    df_test = df_test.sample(frac=1, random_state=seed).reset_index(drop=True)
+
+    y_test = df_test["is_train"].values
+    df_test = df_test.drop(columns=["is_train"])
 
     return df_val, y_val, df_test, y_test
