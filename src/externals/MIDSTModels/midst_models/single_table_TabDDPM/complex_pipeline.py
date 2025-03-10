bc515a350d7f9c83bf0ba5cdf85a40b8ab7748f8
Yue Qi <yue.qi.chum@ssss.gouv.qc.ca>
2025-03-10 17:11:48 -0400

    Revert changes

--- src/externals/MIDSTModels/midst_models/single_table_TabDDPM/complex_pipeline.py
+++ src/externals/MIDSTModels/midst_models/single_table_TabDDPM/complex_pipeline.py
@@ -1,6 +1,3 @@
-import sys
-sys.path.append(".")
-
 import argparse
 import json
 import os
@@ -17,12 +14,12 @@ from .pipeline_utils import *
 from .tab_ddpm.utils import *
 
 
-def clava_clustering(tables, relation_order, save_dir, configs, force_tables=False):
+def clava_clustering(tables, relation_order, save_dir, configs):
     relation_order_reversed = relation_order[::-1]
     all_group_lengths_prob_dicts = {}
 
     # Clustering
-    if os.path.exists(os.path.join(save_dir, "cluster_ckpt.pkl")) and force_tables==False:
+    if os.path.exists(os.path.join(save_dir, "cluster_ckpt.pkl")):
         print("Clustering checkpoint found, loading...")
         cluster_ckpt = pickle.load(
             open(os.path.join(save_dir, "cluster_ckpt.pkl"), "rb")
@@ -82,7 +79,7 @@ def clava_training(tables, relation_order, save_dir, configs):
         id_cols = [col for col in df_with_cluster.columns if "_id" in col]
         df_without_id = df_with_cluster.drop(columns=id_cols)
         result = child_training(
-            df_without_id, tables[child]["domain"], parent, child, configs,
+            df_without_id, tables[child]["domain"], parent, child, configs
         )
         models[(parent, child)] = result
         pickle.dump(
@@ -92,29 +89,17 @@ def clava_training(tables, relation_order, save_dir, configs):
 
     return models
 
-def clava_fine_tuning(trained_models, new_tables, relation_order, save_dir, configs,
-                      new_diffusion_iterations, new_classifier_iterations):
-    new_models = {}
-    for parent, child in relation_order:
-        print(f"Fine Tuning {parent} -> {child} model from pretrained models")
-        df_with_cluster = new_tables[child]["df"]
-        id_cols = [col for col in df_with_cluster.columns if "_id" in col]
-        df_without_id = df_with_cluster.drop(columns=id_cols)
-        result = child_fine_tuning(
-            trained_models, df_without_id, new_tables[child]["domain"], parent, child, configs,
-            new_diffusion_iterations, new_classifier_iterations
-        )
-        new_models[(parent, child)] = result
-
-    return new_models
-
 
 class CustomUnpickler(pickle.Unpickler):
     def find_class(self, module, name):
         if module.startswith("midst_competition.single_table_ClavaDDPM"):
-            module = module.replace("midst_competition.single_table_ClavaDDPM",
-                                    "midst_models.single_table_TabDDPM", 1)
+            module = module.replace(
+                "midst_competition.single_table_ClavaDDPM",
+                "midst_models.single_table_TabDDPM",
+                1,
+            )
         return super().find_class(module, name)
+
 
 def clava_load_pretrained(relation_order, save_dir):
     models = {}
@@ -123,9 +108,11 @@ def clava_load_pretrained(relation_order, save_dir):
             os.path.join(save_dir, f"models/{parent}_{child}_ckpt.pkl")
         )
         print(f"{parent} -> {child} checkpoint found, loading...")
-        with open(os.path.join(save_dir, f"models/{parent}_{child}_ckpt.pkl"), "rb") as f:
+        with open(
+            os.path.join(save_dir, f"models/{parent}_{child}_ckpt.pkl"), "rb"
+        ) as f:
             models[(parent, child)] = CustomUnpickler(f).load()
-       
+
     return models
 
 
